a5dc4ed1e61fac9f9630b29d69c557f0ae4c0836
maleta21 <matheus20041126@gmail.com>
2025-03-10 10:57:45 -0300

    exe 5

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,109 @@ function exe1(){
     // alert("A subtração é " + sub)
     document.getElementById("sub").innerText = "O resultado é " + sub
 }
+//exercicio 2
+function exe2(){
+    //vamos usar DOM para recuperar dados do html
+    //DOM - Document Object Model
+    //converte texto para numero - Number()
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    let nro3 = Number(document.getElementById("nro3").value)
+    let multiplicacao = nro1 * nro2 * nro3
+    document.getElementById("multiplicacao").innerText = "O resultado é " + multiplicacao
+}
+//exercicio 3
+function exe3(){
+    //vamos usar DOM para recuperar dados do html
+    //DOM - Document Object Model
+    //converte texto para numero - Number()
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    let divisao = nro1 / nro2
+    document.getElementById("divisao").innerText = "O resultado é " + divisao
+}
+//exercicio 4
+function exe4(){
+    //vamos usar DOM para recuperar dados do html
+    //DOM - Document Object Model
+    //converte texto para numero - Number()
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    //media ponderada
+    let mp = ((nro1 * 2) + (nro2 * 3)) / 5
+    document.getElementById("mp").innerText = "O resultado é " + mp
+}
+//exercicio 5
+function exe5(){
+    //vamos usar DOM para recuperar dados do html
+    //DOM - Document Object Model
+    //converte texto para numero - Number()
+    let preco = Number(document.getElementById("preco").value)
+    let desconto = (preco * 10) / 100
+    document.getElementById("desconto").innerText = "O resultado é "+ (preco - desconto)
+}
+//exercicio 6
+function exe6(){
+    //vamos usar DOM para recuperar dados do html
+    //DOM - Document Object Model
+    //converte texto para numero - Number()
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    //vamos fazer a subtração
+    let sub = (nro1 * 2 + nro2 * 3) / 5
+    //vamos mostrar o resultado do usuario
+    // alert("A subtração é " + sub)
+    document.getElementById("sub").innerText = "O resultado é " + sub
+}
+//exercicio 7
+function exe7(){
+    //vamos usar DOM para recuperar dados do html
+    //DOM - Document Object Model
+    //converte texto para numero - Number()
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    //vamos fazer a subtração
+    let sub = (nro1 * 2 + nro2 * 3) / 5
+    //vamos mostrar o resultado do usuario
+    // alert("A subtração é " + sub)
+    document.getElementById("sub").innerText = "O resultado é " + sub
+}
+//exercicio 8
+function exe8(){
+    //vamos usar DOM para recuperar dados do html
+    //DOM - Document Object Model
+    //converte texto para numero - Number()
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    //vamos fazer a subtração
+    let sub = (nro1 * 2 + nro2 * 3) / 5
+    //vamos mostrar o resultado do usuario
+    // alert("A subtração é " + sub)
+    document.getElementById("sub").innerText = "O resultado é " + sub
+}
+//exercicio 9
+function exe9(){
+    //vamos usar DOM para recuperar dados do html
+    //DOM - Document Object Model
+    //converte texto para numero - Number()
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    //vamos fazer a subtração
+    let sub = (nro1 * 2 + nro2 * 3) / 5
+    //vamos mostrar o resultado do usuario
+    // alert("A subtração é " + sub)
+    document.getElementById("sub").innerText = "O resultado é " + sub
+}
+//exercicio 10
+function exe10(){
+    //vamos usar DOM para recuperar dados do html
+    //DOM - Document Object Model
+    //converte texto para numero - Number()
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    //vamos fazer a subtração
+    let sub = (nro1 * 2 + nro2 * 3) / 5
+    //vamos mostrar o resultado do usuario
+    // alert("A subtração é " + sub)
+    document.getElementById("sub").innerText = "O resultado é " + sub
+}
